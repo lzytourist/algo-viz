f7f46e239b83520d23a56e2a05a0d505ce2ccbc9
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-05-16 22:21:34 +0600

    Bubble sort

--- algo-viz-next/src/components/algorithms/sorting/bubble-sort.tsx
+++ algo-viz-next/src/components/algorithms/sorting/bubble-sort.tsx
@@ -6,12 +6,16 @@ import {Button} from "@/components/ui/button";
 const BubbleSort = () => {
     const [array, setArray] = useState([1, 2, 3, 4, 5, 6, 7]);
     const [sorting, setSorting] = useState(false);
+    const [cmpIndex, setCmpIndex] = useState(-2);
+    const [index, setIndex] = useState(-2);
 
     const getRandomNumber = (start: number, end: number) => {
         return Math.ceil(Math.random() * (end - start) + end);
     };
 
     const generateArray = () => {
+        setIndex(-2);
+
         let tempArray = [];
         let len = getRandomNumber(5, 10);
 
@@ -29,17 +33,27 @@ const BubbleSort = () => {
 
         for (let i = 0; i < len - 1; i++) {
             for (let j = 0; j < len - i - 1; j++) {
+                setCmpIndex(j);
+
+                await new Promise((resolve) => setTimeout(resolve, 900));
+
                 if (tempArray[j] > tempArray[j + 1]) {
-                    await new Promise((resolve) => setTimeout(resolve, 300)); // Delay for visualization
+                    setIndex(j);
+
+                    await new Promise((resolve) => setTimeout(resolve, 700));
+
                     let temp = tempArray[j];
                     tempArray[j] = tempArray[j + 1];
                     tempArray[j + 1] = temp;
+
                     setArray(tempArray.slice());
+                    setIndex(-2);
                 }
             }
         }
 
         setSorting(false);
+        setCmpIndex(-2);
     };
 
 
@@ -48,12 +62,18 @@ const BubbleSort = () => {
             <div className={'flex justify-center items-end'}>
                 {array.map((value: number, idx: number) => (
                     <div
-                        className={'bg-sky-400 mx-2'}
+                        className={
+                            `mx-2 text-center ${
+                                index == -2 && cmpIndex == idx ? 'bg-orange-200' : 
+                                    index == -2 && cmpIndex + 1 == idx ? 'bg-orange-400' :
+                                        index == idx ? 'bg-red-600' : 'bg-sky-400'
+                            }`
+                        }
                         key={idx}
                         style={{
-                            height: `${value * 10}px`,
-                            width: '20px',
-                            transition: 'background-color 0.3s'
+                            height: `${value * 15}px`,
+                            width: '30px',
+                            transition: 'background-color 0.4s'
                         }}
                     >
                         <span>{value}</span>
