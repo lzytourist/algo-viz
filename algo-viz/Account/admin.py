2a75a5e1433bdd1277269eb4ff0e291fe8d4c9ef
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-06-20 20:26:46 +0600

    Account admin panel

--- algo-viz/Account/admin.py
+++ algo-viz/Account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User, Profile, AccountVerification
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline, ]
+    list_display = ('email', 'first_name', 'last_name', 'get_gender', 'is_active', 'is_verified')
+    list_filter = ('is_verified', 'is_active', 'profile__gender')
+    search_fields = ('email', 'first_name', 'last_name', 'profile__gender', 'profile__institute')
+
+    def get_gender(self, obj):
+        return obj.profile.gender.capitalize()
+
+    get_gender.short_description = 'Gender'
+
+
+admin.site.register(User, UserAdmin)
